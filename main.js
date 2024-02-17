a04676b171e72a1669d36cac1971a08a6e2a499e
BuildTools <unconfigured@null.spigotmc.org>
2024-02-17 12:55:58 -0800

    Add new movieDetails page. Each <li> in movie list page takes user to movieDetails page. Add fourth property to movieObject, runtime. Display runtime on movieDetails page.

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 let MovieArray = [];
 
-let MovieObject = function (name, status, date) {
+let MovieObject = function (name, status, date, runtime) {
     this.name = name; //name of the movie
     this.status = status; //movie's watch status--Finished, Planning, Paused, Rewatching
     this.date = date; //movie's release date
+    this.runtime = runtime; //movie's runtime, in minutes
 }
 
-MovieArray.push ( new MovieObject("Dune: Part Two", "Planning", "3/1/24")  );
-MovieArray.push ( new MovieObject("G Minus One", "Watched", "12/1/23")  );
-MovieArray.push ( new MovieObject("The Boy and the Heron", "Watched", "12/8/23")  );
+MovieArray.push ( new MovieObject("Dune: Part Two", "Planning", "3/1/24", "165 minutes")  );
+MovieArray.push ( new MovieObject("G Minus One", "Watched", "12/1/23", "125 minutes")  );
+MovieArray.push ( new MovieObject("The Boy and the Heron", "Watched", "12/8/23", "124 minutes")  );
 
 let selectedStatus = "";
 
@@ -24,11 +25,12 @@ document.addEventListener("DOMContentLoaded", function (event) {
     document.getElementById("buttonAdd").addEventListener("click", function () {
         //update the MovieArray by pushing a new movie object with the following parameters
         MovieArray.push ( new MovieObject(document.getElementById("nameInput").value, selectedStatus,
-        document.getElementById("dateInput").value ) );
+        document.getElementById("dateInput").value, document.getElementById("runtimeInput").value ) );
         //Grabs the name, watch status, and release date, respectively
         
         document.getElementById("nameInput").value = ""; //reset the Add a Movie textbox
         document.getElementById("dateInput").value = ""; //reset the Add a Movie textbox
+        document.getElementById("runtimeInput").value = ""; //reset the Add a Movie textbox
 
         createList();
     });
@@ -37,7 +39,6 @@ document.addEventListener("DOMContentLoaded", function (event) {
     $(document).bind("change", "#select-status", function (event, ui) {
         selectedStatus = document.getElementById("select-status").value;
     });
-
 });
 
 
@@ -46,12 +47,30 @@ document.addEventListener("DOMContentLoaded", function (event) {
 function createList() {
     // clear prior data
     var myul = document.getElementById("myul");
+        //^^^Line 68 of index.html
     myul.innerHTML = "";
 
-    MovieArray.forEach(function (element,) {   // use handy array forEach method
+    MovieArray.forEach(function (movie) {   // use handy array forEach method
         var li = document.createElement('li');
-          // added data-role="listview" to the ul in the html
-        li.innerHTML = element.date + ":  " + element.status + "   " + element.name;
-        myul.appendChild(li);
+        var link = document.createElement('a');
+        (function(movie) {
+            link.href = "javascript:movieDetails(" + JSON.stringify(movie) + ");"; //pass in the movie object
+        })(movie);
+        li.innerHTML = movie.status + " --- " + movie.name + " (" + movie.date + ")<br>" + 
+                "<span style='padding-left: 70px;'>Runtime: " + movie.runtime;
+        link.appendChild(li); //wrap list element inside an <a></a>
+        myul.appendChild(link); //put the <a><li></li></a> in the <ul></ul>
     });
+};
+
+//populates the movieDetails page using the passed in movie object
+function movieDetails(movie){
+    window.location.href = "index.html#movieDetails"; //take us to the movieDetails page
+        //^^^Find this at line 128 of index.html
+    document.getElementById("detailsHeader").innerHTML = movie.name; //set the movie name as the header
+        //^^^Find this at line 140 of index.html
+    document.getElementById("detailsStatus").innerHTML = "Watch Status: " + movie.status; //display the watch status
+        //^^^Find this at line 145 of index.html
+    document.getElementById("detailsDate").innerHTML = "Release Date: " + movie.date; //display the release date
+        //^^^Find this at line 146 of index.html
 };
